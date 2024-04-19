8561d58ea2680a687a1e9a644b633bfa5937b921
mauriciocosta404 <mc611641@gmail.com>
2024-04-19 19:55:15 +0100

    feat: adicionando uma transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,25 @@
 import { FastifyInstance } from "fastify";
 import { knex } from "../database";
+import {z} from "zod"
+import {randomUUID} from "node:crypto"
 
 export async function transactionsRoutes (app: FastifyInstance){
-    app.get('/hello', async () => {
-        const transactions = await knex('transactions').select('*');
-      
-        return transactions;
-      })
+    app.post('/', async (request, reply) => {
+       
+        const createTransactionsBodySchema = z.object({
+            title: z.string(),
+            amount: z.number(),
+            type: z.enum(['credit','debit']),
+       }); 
+
+       const {title, amount, type} = createTransactionsBodySchema.parse(request.body);
+    
+        await knex('transactions').insert({
+            id: randomUUID(),
+            title,
+            amount: type === 'credit' ? amount: amount*-1,
+       });
+
+       return reply.status(201).send("created");
+    });
 }
