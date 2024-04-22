0c33deaeb0a37e3fe9231b2a7afeecd7c053a3a6
mauriciocosta404 <mc611641@gmail.com>
2024-04-22 18:40:10 +0100

    feat: adicionando os cookies

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -37,11 +37,23 @@ export async function transactionsRoutes(app: FastifyInstance){
         }); 
 
        const {title, amount, type} = createTransactionsBodySchema.parse(request.body);
+
+       let sessionId = request.cookies.sessionId;
+       
+       if(!sessionId){
+            sessionId = randomUUID();
+
+            reply.cookie('sessionId', sessionId, {
+                path: '/',
+                maxAge: 60 * 60 * 24 * 30, // 30 dias
+            });
+       }
     
         await knex('transactions').insert({
             id: randomUUID(),
             title,
             amount: type === 'credit' ? amount: amount*-1,
+            session_id: sessionId,
         });
 
        return reply.status(201).send("created");
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
 import { env } from './env';
 import { transactionsRoutes } from './routes/transactions';
+import cookie from '@fastify/cookie';
 
 const app = fastify()
 
+app.register(cookie);
 app.register(transactionsRoutes,{
   prefix: "transactions",
 });
