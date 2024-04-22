5a53ebab3f0135c26e10c7b09d31b45603b337a8
mauriciocosta404 <mc611641@gmail.com>
2024-04-22 17:48:19 +0100

    feat: listando transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,24 @@ import {z} from "zod";
 import {randomUUID} from "node:crypto"; 
 
 export async function transactionsRoutes(app: FastifyInstance){
+    app.get('/', async () => {
+        const transactions = await knex('transactions').select('');
+
+        return {transactions}
+    })
+
+    app.get('/:id', async (request) => {
+        const getTransactionParamsSchema = z.object({
+            id: z.string().uuid(),
+        });
+
+        const {id} = getTransactionParamsSchema.parse(request.params);
+
+        const transaction = await knex('transactions').where('id',id).first();
+
+        return {transaction}
+    })
+    
     app.post('/', async (request, reply) => {
        
         const createTransactionsBodySchema = z.object({
