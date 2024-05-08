7ee775b8eeea21e1c7019af5d15a84ada1a40198
mauriciocosta404 <mc611641@gmail.com>
2024-05-08 18:34:33 +0100

    chore: adicionando mais configurações para o buid

--- src/database.ts
+++ src/database.ts
@@ -2,10 +2,11 @@ import { env } from "./env";
 import {knex as setupKnex, Knex} from "knex";
 
 export const config : Knex.Config = {
-    client: 'sqlite',
-    connection:{
-        filename: env.DATABASE_URL
-    },
+    client: env.DATABASE_CLIENT,
+    connection: 
+        env.DATABASE_CLIENT==='sqlite' ? {
+            filename: env.DATABASE_URL
+        }:env.DATABASE_URL,
     useNullAsDefault: true,
     migrations: {
         extension:"ts",
